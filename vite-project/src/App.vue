107436c2ab6a8d858e4269fcaf7ec0a3fcc7ab21
KeK <127266806+disneydreamworker@users.noreply.github.com>
2024-10-19 14:19:52 +0900

    vue-lecutre upload

---
+++ vite-project/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    {{ count }}
+  </div>
+</template>
+
+<script>
+export default {
+
+
+  data () {
+    return {
+      count : 0,
+
+    }
+  },
+
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
+
+routes : [
+  {
+    path:'/user/john,
+    name:'userJohn',
+    component: UserView
+  },
+  {
+    path:'/user/jane,
+    name:'userJane',
+    component: UserView
+  },
+  {
+    path:'/user/:id,
+    name:'user',
+    component: UserView
+  },
+]
